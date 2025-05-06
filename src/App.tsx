0bb5e442bb76f13c61b5dab459ad41354da452f3
Jon Narine <jon.wolf.narine@gmail.com>
2025-05-06 13:53:51 -0400

    adds contentRail scaffolding with test data

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import  { Navbar } from './components/Navbar';
 import Slider from './components/Slider';
+import ContentRail from './components/ContentRail';
 
 import styled from "styled-components";
 
+import { mockItems } from './assets/content';
+
 function App() {
   const sliderItems = ['gutar', 'piano', 'ibanez', 'moog', 'roland', 'piano', 'ibanez', 'moog', 'roland', 'gutar', 'piano', 'ibanez', 'moog', 'roland', 'piano', 'ibanez', 'moog', 'roland', 'ibanez', 'moog', 'roland', 'piano', 'ibanez', 'moog', 'roland'];
+
+
   return (
     <Container>
       <Navbar />
       <Slider title='Save an avg' items={sliderItems} />
+      <ContentRail title="Most watched Guitars" items={mockItems} />
     </Container>
   );
 };
